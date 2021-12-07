ff28e3ea270d236957cf8855e8782b8f2c3455e9
Evelyn Acevedo <alwaysevelyn14@gmail.com>
2021-12-07 10:51:05 -0500

    Implemented CitySearch

--- src/tests/CitySearch.test.js
+++ src/tests/CitySearch.test.js
@@ -3,16 +3,59 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
+import { mockData } from '../mock-data';
+import { extractLocations } from '../api';
 
 describe('<CitySearch />, component', () => {
     let CitySearchWrapper;
     beforeAll(() => {
         CitySearchWrapper = shallow(<CitySearch />)
     })
+    // Does the CitySearch component have a text input with the class name "city"?
     test('render text input', ()=> {
         expect(CitySearchWrapper.find('.city')).toHaveLength(1);
     })
+    // Does the CitySearch component have a list element with the class name "suggestions"
     test('renders a list of suggestions', () => {
         expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
     })
+    test('renders text input correctly', () => {
+        const query = CitySearchWrapper.state('query');
+        expect(CitySearchWrapper.find('.city').prop('value')).toBe(query);
+    })
+    test('change state when text input changes', () => {
+        CitySearchWrapper.setState({
+            query: 'Munich'
+        });
+        const eventObject = { target: { value: 'Berlin' }};
+        CitySearchWrapper.find('.city').simulate('change', eventObject);
+        expect(CitySearchWrapper.state('query')).toBe('Berlin');
+    })
+    test('render list of suggestions correctly', () => {
+        const locations = extractLocations(mockData);
+        CitySearchWrapper.setState({ suggestions: locations });
+        const suggestions = CitySearchWrapper.state('suggestions');
+        expect(CitySearchWrapper.find('.suggestions li')).toHaveLength(suggestions.length + 1);
+        for (let i = 0; i < suggestions.length; i += 1) {
+          expect(CitySearchWrapper.find('.suggestions li').at(i).text()).toBe(suggestions[i]);
+        }
+      });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
