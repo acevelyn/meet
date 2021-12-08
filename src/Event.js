e2249a78407530475fe4fe4afb1a52a8efbfb343
Evelyn Acevedo <alwaysevelyn14@gmail.com>
2021-12-08 17:14:02 -0500

    Creating & Testing Event Component

--- src/Event.js
+++ src/Event.js
@@ -2,8 +2,46 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+    state = {
+        collapsed: true,
+    }
+
+    handleButtonClick = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
+
     render(){
-        return <div></div>
+        const { event } = this.props;
+        const { collapsed } = this.state;
+        return (
+        <div className="Event">
+            <h2 className="summary">{event.summary}</h2>
+            <p className="start-date">{event.start.dateTime}</p>
+            <p className="location">{event.location}</p>
+
+            {this.state.collapsed === false && (
+                <div className="more-details">
+                 <p className="event-description">{event.description}</p>
+                 <button 
+                    className="toggleEvent"
+                    onClick={this.handleButtonClick}>
+                        Hide Details
+                </button>
+                 </div>
+            )}
+            {this.state.collapsed === true && (
+                <button 
+                 className="toggleEvent"
+                 onClick={this.handleButtonClick}>
+                     Show Details
+             </button>
+            )}
+        </div>
+        )
     }
 }
 export default Event;
+
+// make a component called EventDetails?
