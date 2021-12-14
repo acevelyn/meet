31389d242cdd7ff5fd8a8292aa3c3badbdf828b7
Evelyn Acevedo <alwaysevelyn14@gmail.com>
2021-12-14 13:43:30 -0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -45,7 +45,7 @@ const removeQuery = () => {
  */
 
   // EXTRACT LOCATIONS
-  const extractLocations = (events) => {
+  export const extractLocations = (events) => {
     var extractLocations = events.map((event) => event.location);
     var locations = [...new Set(extractLocations)];
     return locations;
