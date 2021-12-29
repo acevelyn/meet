51498622b3dae28812a9d8c978302b1b8016477e
Evelyn Acevedo <alwaysevelyn14@gmail.com>
2021-12-29 18:28:00 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,34 +12,19 @@ class App extends Component {
   state = {
     events: [],
     locations: [],
+    currentLocation: 'all',
     numberOfEvents: 32
-    
-  }
-  
-  componentDidMount() {
-    this.mounted = true;
-    const accessToken = localStorage.getItem('access_token');
-    getEvents().then((events) => {
-      if (this.mounted) {
-      this.setState({ 
-        events, 
-        locations: extractLocations(events) });
-      }
-    });
-  }
-
-  componentWillUnmount() {
-     this.mounted = false;
-  }
+    }
   
 
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = (location === 'all' || !location) ?
+      const locationEvents = (location === 'all') ?
       events : 
       events.filter((event)=> event.location === location);
       this.setState({ 
-        events: locationEvents.slice(0, this.state.numberOfEvents)
+        events: locationEvents.slice(0, this.state.numberOfEvents),
+        currentLocation: location
       })
     });
     }
@@ -49,9 +34,24 @@ class App extends Component {
       this.setState({
         numberOfEvents: newNum
       });
-      this.updateEvents(this.state.locations);
+      this.updateEvents(this.state.currentLocation);
     }
 
+    async componentDidMount() {
+      this.mounted = true;
+      const accessToken = localStorage.getItem('access_token');
+      getEvents().then((events) => {
+        if (this.mounted) {
+        this.setState({ 
+          events, 
+          locations: extractLocations(events) });
+        }
+      });
+    }
+  
+    componentWillUnmount() {
+       this.mounted = false;
+    }
 
 
   render() {
