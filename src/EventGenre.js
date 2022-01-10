6295c6e99cabdaa7703d52ac74bd6197cba84ac9
Evelyn Acevedo <alwaysevelyn14@gmail.com>
2022-01-10 14:52:26 -0500

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -21,7 +21,7 @@ const EventGenre = ({events}) => {
         <ResponsiveContainer height={400} >
             <PieChart width={400} height={400}>
                 <Pie
-                    data={this.getData()}
+                    data={data}
                     cx={200}
                     cy={200}
                     labelLine={false}
