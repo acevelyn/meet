c444c89879c0e13b1ba14221f74352fab50e4619
Evelyn Acevedo <alwaysevelyn14@gmail.com>
2022-01-31 17:35:39 -0500

    Update EventGenre.js

--- src/components/EventGenre.js
+++ src/components/EventGenre.js
@@ -6,16 +6,48 @@ const EventGenre = ({events}) => {
 
     useEffect(() => { setData(() => getData()); }, [events]);
 
+
     const getData = () => {
-        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-        const data = genres.map((genre) => {
+      const genres = [
+        'React', 
+        'Javascript', 
+        'Node', 
+        'jQuery', 
+        'AngularJS',
+        'Angular'
+      ];
+
+      const data = genres.reduce(
+        (accumalator, genre) => {
           const value = events.filter(({ summary }) =>
-            summary.split(' ').includes(genre)
-          ).length;
-          return { name: genre, value: value };
-        });
-        return data;
-      };
+             summary.split(' ').includes(genre)
+           ).length;
+
+           if(value !== 0) {
+              accumalator.push({ name: genre, value: value })
+           }
+
+           return accumalator;
+        },
+        []
+      );
+
+      return data;
+    };
+
+    // const getData = () => {
+    //     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    //     const data = genres.map((genre) => {
+    //       const value = events.filter(({ summary }) =>
+    //         summary.split(' ').includes(genre)
+    //       ).length;
+    //       return { name: genre, value: value };
+    //     })
+    //     .filter(({ value }) => {
+    //       return value !== 0
+    //     });
+    //     return data;
+    //   };
 
     return (
         <ResponsiveContainer height={400} >
